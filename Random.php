a092d02a8b90f55b48947cb4faeb3811eeb7fbfc
Corentin Dessenne <56958597+Corentin-Dessenne@users.noreply.github.com>
2020-05-27 14:02:06 +0200

    Add files via upload

--- Random.php
+++ Random.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
+include("connexion.php");
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
 	<meta charset="utf8">
 	<link rel="stylesheet" href="home.css">
@@ -14,31 +15,98 @@ session_start();
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<title></title>
+	<title>Random creation - Picross</title>
 	<style type="text/css">
-	table, th, td {
-		border:1px solid black;
-	}
-	.case {
-		width: 20px;
-		height: 20px;
-	}
+		table, th, td {
+			border:1px solid black;
+		}
+		.case {
+			width: 20px;
+			height: 20px;
+		}
 	</style>
 </head>
 <body>
+	<div class="naviguation">
+		<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+			<a class="navbar-brand" href="Home.php">
+				<img src="./pictures/Picross.jpg" width="50" height="50" alt="">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link waves-effect waves-light" href="Home.php">Home<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link waves-effect waves-light" href="jouer.php">Play</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link waves-effect waves-light" href="create.php">Create</a>
+					</li>
+					<?php if(isset($_SESSION["utilisateur"])){
+						$user = $_SESSION['utilisateur'];
+						$query = "SELECT Name FROM comptes WHERE email = '$user'";
+						$resultat = mysqli_query($link, $query);
+						$result_rows = mysqli_fetch_assoc($resultat);
+						echo "<li class='nav-item dropdown'>";
+						echo "<a class='nav-link dropdown-toggle waves-effect waves-light' data-toggle='dropdown'>".$result_rows['Name']."</a>";
+						echo "<div class='dropdown-menu'>";
+						echo "<a class='dropdown-item' href='Account.php'>My Account</a>";
+						echo "<a class='dropdown-item' href='deconnexion.php'>Log out</a>";
+						echo "</div>";
+						echo "</li>";
+					}
+					if(!isset($_SESSION["utilisateur"])): ?>
+						<li class="nav-item">
+							<a class="nav-link waves-effect waves-light" href="loginPage.php">Login</a>
+						</li>
+					<?php endif ?>
+				</ul>
+				<div class="md-form my-0">
+					<button type="button" class="btn text-primary" data-toggle="modal" data-target="#myModal" style="width: 10em;">Rules</button>
+					<div class="modal fade" id="myModal">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<!-- Header -->
+								<div class="modal-header">
+									<h4 class="modal-title">Rules of the game</h4>
+									<button type="button" class="close" data-dismiss="modal">&times;</button>
+								</div>
+								<!-- body -->
+								<div class="modal-body">
+									<p style="text-align: justify;">A Picross is a puzzle game where the goal is to guess what image is hidden behind a grid. The numbers around the grid are the hints that can help you find which blocks are black, and which are white. Each number tells you how many continuous black blocks exist in this row or column. For example, if a row has the numbers 5 10 this means that this row contains 5 black blocks in a row, 10 black blocks in a row and ANY number of white blocks between them</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+
+	<div class="jumbotron jumbotron-fluid rounded">
+		<div class="container text-center">
+			<h1>Random generation</h1>
+			<p>Generate a random Picross by typing the number of columns, rows and the difficulty you'd like.</p>
+		</div>
+	</div>
+
 	<?php 
 	if(!isset($_POST['ligne']) && !isset($_POST['colonne']) && !isset($_POST['difficulte'])){  // formulaire pour connaitre la taille et/ou la difficulté
-		echo'<p>Tout types de réponses sont acceptés, mais certaines peuvent ne pas aboutir à une grille dans ce cas il sera affiché "Rien".</p>';
+		echo'<p>All inputs are accepted, but some may not result into an outcome, in which case it will be displayed "Nothing"</p>';
 		echo"<form method='post' action='Random.php'>";
-			echo"<fieldset>";
-				echo"<label for='ligne'>Lignes</label> :";
-				echo"<input type='text' name='ligne'/>";
-				echo"<label for='colonne'>Colonnes</label> :";
-				echo"<input type='text' name='colonne'/>";
-				echo"<label for='difficulte'>Difficulté</label> :";
-				echo"<input type='text' name='difficulte'/>";
-				echo"<input type='submit' value='Valider' />";
-			echo"</fieldset>";
+		echo"<fieldset>";
+		echo"<label for='ligne'>Rows</label> :";
+		echo"<input type='text' name='ligne' class='ml-2'/>";
+		echo"<label for='colonne' class='ml-2'>Columns</label> :";
+		echo"<input type='text' name='colonne' class='ml-2'/>";
+		echo"<label for='difficulte' class='ml-2'>Difficulty</label> :";
+		echo"<input type='text' name='difficulte' class='ml-2'/>";
+		echo"<input type='submit' value='Generate' class='ml-2'/>";
+		echo"</fieldset>";
 		echo"</form>";
 	}
 	
@@ -59,10 +127,7 @@ session_start();
 		}
 
 		$retour=[];
-	
-		
-		
-		
+
 		class picross{  // Structure picross qui résume toutes les informations liées à la grille
 			public $lignes;
 			public $colonnes;
@@ -77,9 +142,9 @@ session_start();
 		exec('Executables\Random.exe '.$ligne.' '.$colonne.' '.$difficulte.' ', $retour);  // Exécution du programme random en C
 
 		if($retour==null || (!isset($retour[2]))){  // Si l'éxécution a été un échec
-			echo"<p>Rien</p>";
-		}
-		else{
+		echo"<p>Rien</p>";
+	}
+	else{
 			$random = new picross();  // On crée une nouvelle grille
 			$random->nb_ligne=$retour[0];
 			$random->nb_colonne=$retour[1];
@@ -99,8 +164,8 @@ session_start();
 			
 			$random->nb_case=$retour[($random->nb_ligne+$random->nb_colonne+($random->nb_ligne*$random->nb_colonne))+2]; // On récupère le nombre de case à noircir
 			
-			echo"<p>".$random->nb_case."/".$random->nb_ligne*$random->nb_colonne."</p>";
-			echo"<table>"; // On affiche tout dans un tableau
+			echo"<p class='text-center mt-4'>You have ".$random->nb_case." boxes to fill over ".$random->nb_ligne*$random->nb_colonne."</p>";
+			echo"<table style='margin-left: 45%;'>"; // On affiche tout dans un tableau
 			echo"<tr>";
 			echo"<th class='case'></th>";
 			for($i=0; $i<$random->nb_colonne; $i++){
